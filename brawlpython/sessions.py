4b03540501eea216518481f5d72cdae7e512d807
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-10-09 15:37:06 +0300

    sessions: Session: update _raise_for_status(), _start_attempt_cycle(), __init__(new loop attr), add _reqresp_handler(),

--- brawlpython/sessions.py
+++ brawlpython/sessions.py
@@ -2,7 +2,7 @@
 
 
 from aiohttp import ClientSession, TCPConnector, ClientTimeout
-from asyncio import get_event_loop, sleep
+from asyncio import get_event_loop, sleep, gather, ensure_future
 
 from typing import (
     Any,
@@ -25,6 +25,7 @@ from typing import (
 from .abc import (AbcSession, AbcAsyncInit, AbcAsyncWith, AbcRequest,
                   AbcResponse)
 from .api_toolkit import DEFAULT_HEADERS
+from .exceptions import WITH_CODE, UnexpectedResponseCode
 from .helpers import json
 from .typedefs import (STRS, JSONSEQ, JSONTYPE, JSONS, ARGS,
                        NUMBER, BOOLS, STRJSON, AKW, STRBYTE)
@@ -38,10 +39,14 @@ class Session(AbcSession, AbcAsyncInit, AbcAsyncWith):
     async def __init__(self, repeat_failed: int = 3,
                        timeout: NUMBER = 30,
                        trust_env: bool = True,
+                       loop: Optional[asyncio.AbstractEventLoop] = None,
                        request_class: AbcRequest = Request,
                        response_class: AbcResponse = Response,
-                       success_codes: Container[int] = (200,)):
-        loop = get_event_loop()
+                       success_codes: Container[int] = (200,)) -> None:
+
+        if loop is None:
+            loop = get_event_loop()
+
         self._session = ClientSession(
             connector=TCPConnector(use_dns_cache=False, loop=loop),
             loop=loop,
@@ -78,6 +83,57 @@ class Session(AbcSession, AbcAsyncInit, AbcAsyncWith):
         """
         return self.session.closed
 
+    def _raise_for_status(self, url: str, code: int,
+                          data: Union[JSONTYPE, str]) -> None:
+
+        if isinstance(data, str):
+            message = data if len(data) > 0 else "no message"
+        else:
+            reason = data.get("reason", "")
+            message = data.get("message", "")
+
+            if len(reason) + len(message) == 0:
+                message = "no message"
+
+            if len(reason) > 0 and len(message) > 0:
+                reason += "; "
+
+            message = reason + message
+
+        excp = next(filter(lambda excp: excp.code == code, WITH_CODE), None)
+        if excp is not None:
+            raise excp(url, message)
+        else:
+            raise UnexpectedResponseCode(url, code, message)
+
+    async def _reqresp_handler(self, attempt, i, req, resp):
+        # recent attempts with this req have been unsuccessful
+        if resp is None:
+            resp = await req.send()
+            if resp.code in self._success_codes:
+                # set current resp to successful
+                self._reqresps[i][1] = resp
+            elif attempt == 0:  # last attempt
+                self._raise_for_status(req.url, resp.code, resp.data)
+            else:
+                self._failure_counter += 1
+
+    async def _start_attempt_cycle(self):
+        for attempt in self._attempts:
+            tasks = []
+            self._failure_counter = 0
+            for i, (req, resp) in enumerate(self._reqresps):
+                tasks.append(ensure_future(
+                    self._reqresp_handler(attempt, i, req, resp)))
+
+            await gather(*tasks)
+
+            if self._failure_counter == 0:
+                # collect resps
+                result = [resp for req, resp in self._reqresps]
+                self._reqresps.clear()
+                return result
+
     def one_get(self, url: str, to_json: bool = True,
                 headers: JSONTYPE = {}) -> JSONTYPE:
         req = self._request_class(url, session=self._session,
@@ -85,31 +141,7 @@ class Session(AbcSession, AbcAsyncInit, AbcAsyncWith):
                                   to_json=to_json, headers=headers)
 
         self._reqresps.append([req, None])
-        return self._attempt_cycle
-
-    def _raise_for_status(self):
-        raise Exception("test run")
-
-    async def _start_attempt_cycle(self):
-        for attempt in self._attempts:
-            failure_counter = 0
-            for i, (req, resp) in enumerate(self._reqresps):
-                # recent attempts with this req have been unsuccessful
-                if resp is None:
-                    resp = await req.send()
-                    if resp.code in self._success_codes:
-                        # set current resp to successful
-                        self._reqresps[i][1] = resp
-                    elif attempt == 0:  # last attempt
-                        self._raise_for_status
-                    else:
-                        failure_counter += 1
-
-            if failure_counter == 0:
-                # collect resps
-                result = [resp for req, resp in self._reqresps]
-                self._reqresps.clear()
-                return result
+        return self._attempt_cycle()
 
 
 class Response(AbcAsyncInit, AbcResponse):
