1d4ceea03ac866ef6826095c88ef87f1bf69a44b
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-10-15 05:14:12 +0300

    typedefs: rearrange values, rename some of them

--- brawlpython/typedefs.py
+++ brawlpython/typedefs.py
@@ -1,67 +1,46 @@
 # -*- coding: utf-8 -*-
 
-from typing import (
-    Any,
-    Callable,
-    Coroutine,
-    Dict,
-    Generator,
-    Generic,
-    Iterable,
-    List,
-    Mapping,
-    Optional,
-    Sequence,
-    Set,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-)
-from .abc import AbcRequest, AbcResponse
+from typing import (Any, Callable, Coroutine, Dict, Generator, Generic,
+                    Iterable, List, Mapping, Optional, Sequence, Set, Tuple,
+                    Type, TypeVar, Union)
 
+from .abc import AbcRequest, AbcResponse
 
 __all__ = (
     "JSONV",
-    "JSONTYPE",
-    "JSONSEQ",
+    "JSONT",
+    "BOTH_JSON",
+    "STRJSON",
+    "DICT_STR",
+    "MAPPING_STR",
     "STRS",
     "BOOLS",
-    "JSONS",
-    "HANDLER",
     "NUMBER",
     "INTSTR",
-    "AKW",
     "STRBYTE",
-    "REQRESP")
+    "ARGS",
+    "AKW",
+    "REQRESP",
+    "HANDLER")
 
 # SEE: https://github.com/python/typing/issues/182
-JSONVALS = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
-JSONTYPE = Union[Dict[str, JSONVALS], List[JSONVALS]]
+JSONV = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
+JSONT = Union[Dict[str, JSONV], List[JSONV]]
+BOTH_JSON = Union[JSONT, Sequence[JSONT]]  # FIXME: better not to use this
+STRJSON = Union[str, JSONT]
 
-JSONSEQ = Sequence[JSONTYPE]
-JSONS = Union[JSONTYPE, JSONSEQ]
-
-STRJSON = Union[str, JSONTYPE]
-
-STRDICT = Dict[str, str]
-
-# Tuple[]
+DICT_STR = Dict[str, str]
+MAPPING_STR = Mapping[str, str]
 
 STRS = Union[Sequence[str], str]
-
 BOOLS = Union[Sequence[bool], bool]
 
-HANDLER = Callable[["AsyncClient", JSONSEQ], JSONSEQ]
-
 NUMBER = Union[int, float]
-
 INTSTR = Union[int, str]
-
-ARGS = Sequence[Any]
-
-AKW = Tuple[ARGS, Mapping[str, Any]]
-
 STRBYTE = Union[str, bytes]
 
+ARGS = Sequence[Any]
+AKW = Tuple[ARGS, Mapping[str, Any]]
+
+HANDLER = Callable[["AsyncClient", Sequence[JSONT]], Sequence[JSONT]]
 REQRESP = Tuple[Optional[AbcRequest], Optional[AbcResponse]]
